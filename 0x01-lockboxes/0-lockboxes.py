317a3b0cb7246d370bf8d285d50ef592910e46b7
Abdellatif <abdelt.2012@gmail.com>
2024-06-06 22:18:04 +0100

    Done.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+def canUnlockAll(boxes):
+    # Create a set to store the keys we have
+    keys = set([0])
+
+    # Iterate while we have keys
+    while keys:
+        # Take a key from the set
+        key = keys.pop()
+
+        # If the key corresponds to a box
+        if key < len(boxes):
+            # Open the box and add the keys to our set
+            keys.update(boxes[key])
+
+            # Empty the box
+            boxes[key] = []
+
+    # Check if all boxes are empty (i.e., all boxes have been opened)
+    return all(not box for box in boxes)
